e866bcbc8866b898936728b99581792943a73e02
YEM丶星河 <584534417@qq.com>
2023-08-08 19:39:16 +0800

    实现加入购物车的弹窗

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import { Button, Tabbar, TabbarItem, NavBar, Toast, Search, Swipe, SwipeItem, Grid, GridItem, Icon, Rate } from 'vant'
+import { Button, Tabbar, TabbarItem, NavBar, Toast, Search, Swipe, SwipeItem, Grid, GridItem, Icon, Rate, ActionSheet } from 'vant'
 
+Vue.use(ActionSheet)
 Vue.use(Icon)
 Vue.use(Toast)
 Vue.use(Button)
